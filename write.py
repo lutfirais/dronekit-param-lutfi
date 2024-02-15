bec669bf0d8a216071dac88ca868edf2e07870cc
bercitacita <bercitacita123@gmail.com>
2024-02-15 20:20:16 +0700

    fixing bug yang bikin gak bisa autorun

--- write.py
+++ write.py
@@ -1,12 +1,57 @@
-from pathlib import Path
+from dronekit import connect, VehicleMode
 import time
+import argparse
+import RPi.GPIO as GPIO
+from pathlib import Path
 
-file_name = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+#vehicle = connect('127.0.0.1:14550',wait_ready=True)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW) #16
+GPIO.output(16, GPIO.HIGH)
+
+vehicle = connect('/dev/ttyAMA0',baud=57600,wait_ready = True)
+# SITL 127.0.0.1:14550
+# Raspberry Pi -> Pixhawk /dev/ttyAMA0 baud =57600
+
+last_attitude_cache = None
+t = time.time()
+
+# Print Header
+local_time = time.localtime()
+incTime = 0
 
 # Making .txt log file
-Path(f'./Log/{file_name}.txt').touch()
+file_name = time.strftime("%Y-%m-%d_%H:%M:%S",time.localtime()) 
+Path(f'/home/drone/dronekit-param/Log/{file_name}.txt').touch()
+f = open(f"/home/drone/dronekit-param/Log/{file_name}.txt","a")
+f.write(f"deltaTime(s);Time(s);Roll(rad);Pitch(rad);Yaw(rad);RollSpeed(rad/s);PitchSpeed(rad/s);YawSpeed(rad/s) \n")
 
-f = open(f"./Log/{file_name}.txt","a")
+# Callback function that prints attitude info 
+# Big thanks to ajayaru <3
+def attitude_callback(self, attr_name, value):
+    global last_attitude_cache
+    global t
+    global incTime
+    input_string = str(value)
+    GPIO.output(16, GPIO.LOW) 
 
-for i in range(10):
-    f.write("HELLO !!! \n")
+    # Split the string based on commas
+    split_values = input_string.split(',')
+
+    # Extract pitch, yaw, and roll values
+    pitch = float(split_values[0].split('=')[1])
+    yaw = float(split_values[1].split('=')[1])
+    roll = float(split_values[2].split('=')[1])
+
+    deltaTime = time.time() - t
+    t = time.time()
+    incTime += deltaTime
+    # print("paket received !")
+    f.write(f"%.8f;%.8f;%.8f;%.8f;%.8f;%.8f;%.8f;%.8f \n" %(deltaTime,incTime,roll,pitch,yaw,vehicle._rollspeed,vehicle._pitchspeed,vehicle._yawspeed))
+    last_attitude_cache=value
+
+# Adding Callback
+vehicle.add_attribute_listener('attitude', attitude_callback)
+while True:
+    continue
