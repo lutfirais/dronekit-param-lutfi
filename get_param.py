70908a40c59103b6246040d3bbe2813bab7701dd
fdeedat <fdeedat969@gmail.com>
2024-02-17 16:08:37 +0700

    added pwm readability

--- get_param.py
+++ get_param.py
@@ -1,24 +1,39 @@
 from dronekit import connect, VehicleMode
 import time
 import argparse  
+from pathlib import Path
+
+# Making .txt log file
+# file_name = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) 
+# Path(f'./Log/{file_name}.txt').touch()
+# f = open(f"./Log/{file_name}.txt","a")
 
 vehicle = connect('127.0.0.1:14550',wait_ready=True)
+# SITL 127.0.0.1:14550
+# Raspberry Pi -> Pixhawk /dev/ttyAMA0 baud =57600
+
 last_attitude_cache = None
 t = time.time()
 
 # Print Header
 local_time = time.localtime()
 incTime = 0
-print("Current Time : ",time.strftime("%Y-%m-%d %H:%M:%S",local_time))
-print(f"deltaTime(s) \t Time(s) \t Roll(rad) \t Pitch(rad) \t Yaw(rad) \t RollSpeed(rad/s) \t PitchSpeed(rad/s) \t YawSpeed(rad/s) ")
+# f.write(f"deltaTime(s) \t Time(s) \t Roll(rad) \t Pitch(rad) \t Yaw(rad) \t RollSpeed(rad/s) \t PitchSpeed(rad/s) \t YawSpeed(rad/s) \n")
+# For debugging purposes
+print(f"deltaTime(s)\tTime(s)\tRoll(rad)\tPitch(rad)\tYaw(rad)\tRollSpeed(rad/s)\tPitchSpeed(rad/s\tYawSpeed(rad/s)\tMotor1(PWM)\tMotor2(PWM)\tMotor3(PWM)\tMotor4(PWM)\t\n")
 
-# Callback function that prints attitude info 
-# Big thanks to ajayaru <3
-def attitude_callback(self, attr_name, value):
-    global last_attitude_cache
+# Listening to SERVO_OUTPUT_RAW mavlink topic
+@vehicle.on_message('SERVO_OUTPUT_RAW') 
+def listener(vehicle, name, m):
+    # print(f"{m.servo1_raw}\t{m.servo2_raw}\t{m.servo3_raw}\t{m.servo4_raw}") # The Four PWM of the Quadcopter
     global t
     global incTime
-    input_string = str(value)
+    
+    deltaTime = time.time() - t
+    t = time.time()
+    incTime += deltaTime
+
+    input_string = str(vehicle.attitude)
 
     # Split the string based on commas
     split_values = input_string.split(',')
@@ -28,13 +43,9 @@ def attitude_callback(self, attr_name, value):
     yaw = float(split_values[1].split('=')[1])
     roll = float(split_values[2].split('=')[1])
 
-    deltaTime = time.time() - t
-    t = time.time()
-    incTime += deltaTime
-    print(f"{deltaTime} \t {incTime} \t {roll} \t {pitch} \t {yaw} \t {float(vehicle._rollspeed)} \t {float(vehicle._pitchspeed)} \t {float(vehicle._yawspeed)} \n")
-    last_attitude_cache=value
+    print(f"%.8f \t%.8f\t%.8f\t%.8f\t%.8f\t%.8f\t%.8f\t%.8f\t%d\t%d\t%d\t%d\n" %(deltaTime,incTime,roll,pitch,yaw,vehicle._rollspeed,vehicle._pitchspeed,vehicle._yawspeed,m.servo1_raw,m.servo2_raw,m.servo3_raw,m.servo4_raw))
 
 # Adding Callback
-vehicle.add_attribute_listener('attitude', attitude_callback)
+# vehicle.add_attribute_listener('attitude', attitude_callback)
 while True:
     continue
