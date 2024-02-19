8d56ea1a408ccd1b76b4a805e8ca8b2089cb512a
bercitacita <bercitacita123@gmail.com>
2024-02-19 19:03:11 +0700

    added gpio

--- get_param.py
+++ get_param.py
@@ -2,6 +2,13 @@ from dronekit import connect, VehicleMode
 import time
 import argparse  
 from pathlib import Path
+import RPi.GPIO as GPIO
+
+#LED Indicator
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW) #16
+GPIO.output(16, GPIO.HIGH)
 
 # Making .txt log file
 file_name = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) 
@@ -19,14 +26,15 @@ t = time.time()
 # Print Header
 local_time = time.localtime()
 incTime = 0
-f.write(f"deltaTime(s)\tTime(s)\tRoll(rad)\tPitch(rad)\tYaw(rad)\tRollSpeed(rad/s)\tPitchSpeed(rad/s)\tYawSpeed(rad/s)\n")
+f.write(f"deltaTime(s)\tTime(s)\tRoll(rad)\tPitch(rad)\tYaw(rad)\tRollSpeed(rad/s)\tPitchSpeed(rad/s)\tYawSpeed(rad/s)\tMotor1(PWM)\tMotor2(PWM)\tMotor3(PWM)\tMotor4(PWM)\t\n")
 
 # Listening to SERVO_OUTPUT_RAW mavlink topic
 @vehicle.on_message('SERVO_OUTPUT_RAW') 
 def listener(vehicle, name, m):
     global t
     global incTime
-    
+    GPIO.output(16, GPIO.LOW)
+
     deltaTime = time.time() - t
     t = time.time()
     incTime += deltaTime
